e39b330f3008f39d65519c3848f6dee3a42fa0e6
Steph <stephonmccarthy@gmail.com>
2019-07-24 15:58:20 -0400

    Added some cool stuff

--- src/components/nav-component.tsx
+++ src/components/nav-component.tsx
@@ -29,6 +29,10 @@ const NavComponent: React.FC = () => {
                             <Link to="/clicker"
                                 className="unset-anchor nav-link">Clicker</Link>
                         </li>
+                        <li className="nav-item active">
+                            <Link to="/coolStuff"
+                                className="unset-anchor nav-link">Cool Guys/Gals Only</Link>
+                        </li>
 
                     </ul>
                 </div>
